6341fdd46443e239b38310cf529e4ceda65c682e
lanbow93 <the.doctor1501@gmail.com>
2023-08-24 18:54:59 -0500

    User Schema created

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,1 +1,16 @@
-// import mongoose from "../db/connection";
+import mongoose from "../db/connection";
+import { IUser } from "../utils/InterfacesUsed";
+
+const {Schema, model} = mongoose
+
+const userSchema = new Schema<IUser>({
+    username: String,
+    password: String,
+    email: String,
+    resetToken: String,
+    resetTokenExpiry: Date
+})
+
+const User = model("User", userSchema)
+
+export default User
