57d04e297948c9c838f51c78f25385f78a4b72b6
lanbow93 <the.doctor1501@gmail.com>
2023-09-02 20:22:57 -0500

    able to access userAccount when verified

--- dist/controllers/userAccount.js
+++ dist/controllers/userAccount.js
@@ -5,6 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const userAccount_1 = __importDefault(require("../models/userAccount"));
+const UserVerified_1 = __importDefault(require("../utils/UserVerified"));
+const SharedFunctions_1 = require("../utils/SharedFunctions");
 const router = express_1.default.Router();
 router.get("/", async (request, response) => {
     try {
@@ -19,6 +21,24 @@ router.get("/", async (request, response) => {
         });
     }
 });
-router.get("/edit:id");
+router.get("/edit/:id", UserVerified_1.default, async (request, response) => {
+    try {
+        const userAccount = await userAccount_1.default.findOne({ accountID: request.params.id });
+        if (userAccount) {
+            if (userAccount.accountID === request.body.requestorID) {
+                (0, SharedFunctions_1.successfulRequest)(response, "Successful Request", "Success", userAccount);
+            }
+            else {
+                (0, SharedFunctions_1.failedRequest)(response, "User._id && Requestor._id Don't Match", "Unable To Access Records", "Authorization Failed");
+            }
+        }
+        else {
+            (0, SharedFunctions_1.failedRequest)(response, "Unable To Locate By accountID", "Account Doesn't Exist", "Unable To Locate");
+        }
+    }
+    catch (error) {
+        (0, SharedFunctions_1.failedRequest)(response, "Unable To Retrieve UserAccount", "Unable To Retrieve Account", { error });
+    }
+});
 exports.default = router;
 //# sourceMappingURL=userAccount.js.map
--- src/controllers/userAccount.ts
+++ src/controllers/userAccount.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import UserAccount from "../models/userAccount";
 import userLoggedIn from "../utils/UserVerified";
-import { failedRequest } from "../utils/SharedFunctions";
+import { failedRequest, successfulRequest } from "../utils/SharedFunctions";
 import { IUserAccount } from "../utils/InterfacesUsed";
 const router: express.Router = express.Router()
 
@@ -19,13 +19,18 @@ router.get("/", async(request: express.Request, response: express.Response) => {
     }
 })
 /*
-Purpose: Creates a new user
-Needed: Params.id = user_id
+Purpose: Get's User Specific Account Information
+Needed: Params.id = userAccount_id | requestorID = user_id
 */
 router.get("/edit/:id", userLoggedIn, async (request: express.Request, response: express.Response) => {
     try{
-        const userAccount: IUserAccount = await UserAccount.findOne({accountID: request.params.id})
+        const userAccount: IUserAccount | null = await UserAccount.findOne({accountID: request.params.id})
         if(userAccount){
+            if(userAccount.accountID === request.body.requestorID){
+                successfulRequest(response, "Successful Request", "Success", userAccount)
+            }else {
+                failedRequest(response, "User._id && Requestor._id Don't Match", "Unable To Access Records", "Authorization Failed")
+            }
         }else {
             failedRequest(response, "Unable To Locate By accountID", "Account Doesn't Exist", "Unable To Locate")
         }
